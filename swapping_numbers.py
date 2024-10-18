ac177a9af7d83839337ddf82512e0a813f29634d
RasoolBux123 <59928743+RasoolBux123@users.noreply.github.com>
2024-10-18 23:15:13 +0500

    Create swapping_numbers.py

---
+++ swapping_numbers.py
@@ -0,0 +1,13 @@
+# Swapping  numbers in Python
+
+# Input: Two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(f"Before swapping: a = {a}, b = {b}")
+
+# Swapping process
+a, b = b, a
+
+# Output: Swapped numbers
+print(f"After swapping: a = {a}, b = {b}")
